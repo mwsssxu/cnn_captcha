b1d47df750ab7eff6a6d5056adfbe83189d957a5
xuning <xuning888@hotmail.com>
2020-07-03 18:49:52 +0800

    chinese

---
+++ keras/predict/predict_classification.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# encoding: utf-8
+import keras
+from keras.preprocessing.image import img_to_array
+import imutils.paths as paths
+import cv2
+import os
+import numpy as np
+import matplotlib.pylab as plt
+
+# import sys
+# sys.path.append("../process")
+# import data_input
+
+channel = 3
+height = 32
+width = 32
+class_num = 62
+norm_size = 32  # 参数
+batch_size = 32
+epochs = 40
+
+test_path = "../data/test"
+image_paths = sorted(list(paths.list_images(test_path)))  # 提取图片
+model = keras.models.load_model("traffic_model.h5")  # 加载模型
+for each in image_paths:
+    image = cv2.imread(each)
+    image = cv2.resize(image, (norm_size, norm_size))  # 读取图片，修改尺寸
+    image = img_to_array(image) / 255.0  # 归一化
+    image = np.expand_dims(image, axis=0)  # 单张图片，改变维度
+    result = model.predict(image)  # 分类预测
+    proba = np.max(result)  # 最大概率
+    predict_label = np.argmax(result)  # 提取最大概率下标
+    label = int(each.split(os.path.sep)[-2])  # 提取图片路径上的标签
+    plt.imshow(image[0])  # 显示
+    plt.title("label:{},predict_label:{}, proba:{:.2f}".format(label, predict_label, proba))
+    plt.show()
+
+# test_x,test_y = data_input.load_data("../data/train",norm_size,class_num)
+
+# model = keras.models.load_model("traffic_model.h5")
+# result = model.predict( ,batch_size=1,verbose=0)
+# print(result)
+# print(result.shape)
---
+++ keras/process/data_input.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from keras.preprocessing.image import img_to_array  # 图片转为array
+from keras.utils import to_categorical  # 相当于one-hot
+from imutils import paths
+import cv2
+import numpy as np
+import random
+import os
+
+
+def load_data(path, norm_size, class_num):
+    data = []  # 数据x
+    label = []  # 标签y
+    image_paths = sorted(list(paths.list_images(path)))  # imutils模块中paths可以读取所有文件路径
+    random.seed(0)  # 保证每次数据顺序一致
+    random.shuffle(image_paths)  # 将所有的文件路径打乱
+    for each_path in image_paths:
+        image = cv2.imread(each_path)  # 读取文件
+        image = cv2.resize(image, (norm_size, norm_size))  # 统一图片尺寸
+        image = img_to_array(image)
+        data.append(image)
+        maker = int(each_path.split(os.path.sep)[-2])  # 切分文件目录，类别为文件夹整数变化，从0-61.如train文件下00014，label=14
+        label.append(maker)
+    data = np.array(data, dtype="float") / 255.0  # 归一化
+    label = np.array(label)
+    label = to_categorical(label, num_classes=class_num)  # one-hot
+    return data, label
